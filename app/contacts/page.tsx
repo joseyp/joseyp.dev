d90b39536ada63831884ed2b894b86dca746922e
Jose Purba <josprb@gmail.com>
2023-09-07 21:24:29 +0700

    updated contact page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -9,8 +9,8 @@ const Contacts = () => {
     <p>Get in touch with me.</p>
     <ul className="mt-3">
       <li>Email - <a href="mailto:josprb@gmail.com" className="text-teal-600 font-semibold underline underline-offset-2">josprb@gmail.com</a></li>
-      <li>Github - <a href="https://github.com/joseprb" className="text-teal-600 font-semibold underline underline-offset-2">https://github.com/joseprb</a></li>
-      <li>LinkedIn - <a href="https://linkedin.com/in/josepurba" className="text-teal-600 font-semibold underline underline-offset-2">https://linkedin.com/in/josepurba</a></li>
+      <li>Github - <a href="https://github.com/joseyp" className="text-teal-600 font-semibold underline underline-offset-2">github.com/joseyp</a></li>
+      <li>LinkedIn - <a href="https://linkedin.com/in/josepurba" className="text-teal-600 font-semibold underline underline-offset-2">linkedin.com/in/josepurba</a></li>
       <li>Discord - <span className="text-teal-600 font-semibold underline underline-offset-2">joseyp</span></li>
     </ul>
   </div>;
