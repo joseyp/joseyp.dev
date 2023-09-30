7505757c86ef7fdbc47115a92ba6125c5ac8f472
Jose Yolanda Purba <josprb@gmail.com>
2023-09-30 12:39:41 +0700

    add target _blank to contact links

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -9,9 +9,9 @@ const Contacts = () => {
   return <div className="w-full flex-grow text-sm lg:text-base">
     <p>Get in touch with me.</p>
     <ul className="mt-3">
-      <li>Email - <a href="mailto:josprb@gmail.com" className="text-teal-600 font-semibold underline underline-offset-2">josprb@gmail.com</a></li>
-      <li>Github - <a href="https://github.com/joseyp" className="text-teal-600 font-semibold underline underline-offset-2">github.com/joseyp</a></li>
-      <li>LinkedIn - <a href="https://linkedin.com/in/josepurba" className="text-teal-600 font-semibold underline underline-offset-2">linkedin.com/in/josepurba</a></li>
+      <li>Email - <a href="mailto:josprb@gmail.com" target="_blank" className="text-teal-600 font-semibold underline underline-offset-2">josprb@gmail.com</a></li>
+      <li>Github - <a href="https://github.com/joseyp" target="_blank" className="text-teal-600 font-semibold underline underline-offset-2">github.com/joseyp</a></li>
+      <li>LinkedIn - <a href="https://linkedin.com/in/josepurba" target="_blank" className="text-teal-600 font-semibold underline underline-offset-2">linkedin.com/in/josepurba</a></li>
       <li>Discord - <span className="text-teal-600 font-semibold underline underline-offset-2">joseyp</span></li>
     </ul>
   </div>;
